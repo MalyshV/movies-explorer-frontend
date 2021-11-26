eae6bb01ae591a349ebe1ee9943f5e24e278b721
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-11-27 02:52:25 +0300

    feat: add mobile & laptop layout for 404 page

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -5,9 +5,7 @@ import Preloader from '../Preloader/Preloader';
 function PageNotFound () {
   return (
     <div className="not-found">
-      <h1 className="not-found__title">
-       <span>404</span>
-      </h1>
+      <h1 className="not-found__title">404</h1>
       <p className="not-found__subtitle">
         Страница не найдена
       </p>
