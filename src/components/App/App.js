757b8cffbd58c890fd26f269cfabe6e4e5641f5d
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-11-27 00:19:13 +0300

    feat: add Preloader & 404 component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import { Route } from 'react-router-dom';
+import PageNotFound from '../PageNotFound/PageNotFound';
+import Preloader from '../Preloader/Preloader';
 
 function App() {
   return (
     <>
-      <Route path='/'></Route>
-      <Route path='/movies'></Route>
-      <Route path='/saved-movies'></Route>
-      <Route path='/profile'></Route>
-      <Route path='/signup'></Route>
-      <Route path='/signin'></Route>
+      <PageNotFound />
+      <Preloader />
     </>
   );
 }
 
 export default App;
+
+/* <Route path='/'></Route>
+      <Route path='/movies'></Route>
+      <Route path='/saved-movies'></Route>
+      <Route path='/profile'></Route>
+      <Route path='/signup'></Route>
+      <Route path='/signin'></Route> */
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function PageNotFound () {
+  return (
+    <div className="not-found">
+      <h1 className="not-found__title">
+       <span>404</span>
+      </h1>
+      <p className="not-found__subtitle">
+        Страница не найдена
+      </p>
+      <Link className="note-found__link" to="/">Назад</Link>
+    </div>
+  )
+}
+
+export default PageNotFound;
