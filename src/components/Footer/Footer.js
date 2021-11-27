f46ddb9b0434369577c145e30c606537d004d63f
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-11-27 15:04:43 +0300

    fix: fix footer mobile layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
       </h3>
       <div className="footer__columns">
         <nav className="footer__links">
-          <Link className="footer__link" to="https://practicum.yandex.ru/" target="_blank">Яндекс.Практикум</Link>
-          <Link className="footer__link" to="https://github.com/MalyshV" target="_blank">Github</Link>
-          <Link className="footer__link" to="https://www.facebook.com/profile.php?id=100013538104816" target="_blank">Facebook</Link>
+          <a className="footer__link" rel="noreferrer" href="https://practicum.yandex.ru/" target="_blank">Яндекс.Практикум</a>
+          <a className="footer__link" rel="noreferrer" href="https://github.com/MalyshV" target="_blank">Github</a>
+          <a className="footer__link" rel="noreferrer" href="https://www.facebook.com/profile.php?id=100013538104816" target="_blank">Facebook</a>
         </nav>
         <p class="footer__copyright">&copy; 2021</p>
       </div>
