c1a54525aa2679a083fcb200d28c85a64f593a3a
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2021-12-10 03:17:47 +0300

    style: cleaned the code a little bit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -3,7 +3,7 @@ import React from "react";
 const FilterCheckbox = () => {
   return (
     <div className="checkbox">
-      <label className="checkbox__name" for="checkbox">Короткометражки</label>
+      <label className="checkbox__name" htmlFor="checkbox">Короткометражки</label>
       <input type="checkbox" id="checkbox" className="checkbox__image" />
     </div>
   )
